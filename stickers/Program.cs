47854327dbeaeea4f9678b61e066e47fe5464290
MPoleto <poleto.mari@gmail.com>
2023-08-24 15:17:39 -0300

    dia 1 - aula

---
+++ stickers/Program.cs
@@ -0,0 +1,35 @@
+﻿using stickers;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Conexão HTTP
+
+string url = "https://raw.githubusercontent.com/alexfelipe/imersao-java/json/top250.json";
+
+
+using (HttpClient client = new())
+using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+using (var response = await client.SendAsync(request, new CancellationToken()))
+{
+  response.EnsureSuccessStatusCode();
+  var content = await response.Content.ReadAsStringAsync();
+  
+
+  // Extrair dados de interesse: titulo, poster, classificação
+  JsonParser parser = new();
+  List<Dictionary<string, string>> ListMovies = parser.ParseJson(content);
+  
+  // Exibir e manipular os dados
+
+  foreach (var movie in ListMovies)
+  {
+    Console.WriteLine($"Título: {movie.GetValueOrDefault("title")}");
+    Console.WriteLine($"Imagem: {movie.GetValueOrDefault("image")}");
+    Console.Write($"Rating: {movie.GetValueOrDefault("imDbRating")}");
+
+    Console.WriteLine("\r\n");
+  }
+}
+
+
+
---
+++ stickers/README.md
@@ -0,0 +1,14 @@
+# Imersão Alura
+
+Projeto desenvolvido a partir da imersão java Alura, mas feito em C#.
+
+## Descrição
+
+### 1º Dia
+
+- Fazer chamada da API, método GET.
+- Armazenar a resposta em uma variável do tipo string
+- Criar uma classe com método que, usando expressões regulares, transforma o json em string e organiza as informações em um dicionário, com o nome do atributo e o valor do atributo.
+- Exibir somente as informações selecionadas: titulo, link imagem e nota dos filmes.
+- API alternativa ao do IMDB, usando o top 10 melhores filmes.
+
