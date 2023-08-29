9a4e0e9424d13d563bdc33ca4d77ee11be927626
MPoleto <poleto.mari@gmail.com>
2023-08-29 17:00:00 -0300

    dia 1 - desafios

--- stickers/Program.cs
+++ stickers/Program.cs
@@ -1,4 +1,5 @@
-﻿using stickers;
+﻿using System.Text.Json;
+using stickers;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
@@ -6,28 +7,98 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 string url = "https://raw.githubusercontent.com/alexfelipe/imersao-java/json/top250.json";
 
-
 using (HttpClient client = new())
 using (var request = new HttpRequestMessage(HttpMethod.Get, url))
 using (var response = await client.SendAsync(request, new CancellationToken()))
 {
   response.EnsureSuccessStatusCode();
   var content = await response.Content.ReadAsStringAsync();
-  
 
   // Extrair dados de interesse: titulo, poster, classificação
+
   JsonParser parser = new();
-  List<Dictionary<string, string>> ListMovies = parser.ParseJson(content);
+  List<Dictionary<string, string>> movies = parser.ParseJson(content);
   
   // Exibir e manipular os dados
-
-  foreach (var movie in ListMovies)
+  
+  foreach (var movie in movies)
   {
-    Console.WriteLine($"Título: {movie.GetValueOrDefault("title")}");
-    Console.WriteLine($"Imagem: {movie.GetValueOrDefault("image")}");
-    Console.Write($"Rating: {movie.GetValueOrDefault("imDbRating")}");
+    Console.WriteLine($"\u001b[42m\u001b[1m{movie.GetValueOrDefault("title"),67} \u001b[m");
+    Console.WriteLine($"Imagem:\u001b[3m {movie.GetValueOrDefault("image")} \u001b[m");
 
+    double rating = double.Parse(movie.GetValueOrDefault("imDbRating")!.Replace('.', ','));
+    int count = 1;
+    Console.Write($"Rating: ");
+    while (rating >= count)
+    {
+      Console.Write("💚 ");
+
+      count++;
+    }
     Console.WriteLine("\r\n");
+    Console.WriteLine("\r\n");
+  }
+
+
+
+  // Desafios:
+
+  var moviesList = JsonSerializer.Deserialize<MoviesList>(content);
+
+  RatingMovie(); 
+
+  foreach (var movie in moviesList.Movie)
+  {
+    Console.WriteLine($"\u001b[42m\u001b[1m {movie.Title, 67} \u001b[m");
+    Console.WriteLine($" Imagem: \u001b[3m {movie.Image} \u001b[m");
+    
+    double rating = double.Parse(movie.Rating!.Replace('.', ','));
+    int count = 1;
+    Console.Write($" Rating:  ");
+    while (rating >= count)
+    {
+      Console.Write(" ⭐");
+
+      count++;
+    }
+
+    Console.WriteLine();
+    Console.Write($" Sua nota: ");
+    count = 1;
+    while (movie.PersonalRating >= count)
+    {
+      Console.Write("💚 ");
+
+      count++;
+    }
+    
+    Console.WriteLine("\r\n");
+    Console.WriteLine("\r\n");
+  }
+
+  void RatingMovie() {
+    Console.WriteLine("Qual sua nota para os filmes?");
+
+    for (var i = 0; i < moviesList.Movie.Count; i++)
+    {
+
+      Console.Write($"{moviesList.Movie[i].Title}: ");
+      int notaPessoal = 0;
+      bool invalid = true;
+      while (invalid)
+      {
+        bool nota = int.TryParse(Console.ReadLine(), out notaPessoal);
+        if (notaPessoal > 0 && notaPessoal <= 10)
+        {
+          invalid = false;
+        }
+        else
+        {
+          Console.WriteLine("Nota inválida. Tente novamente.");
+        }
+      }
+      moviesList.Movie[i].PersonalRating = notaPessoal;
+    }
   }
 }
 
--- stickers/README.md
+++ stickers/README.md
@@ -8,7 +8,11 @@ Projeto desenvolvido a partir da imersão java Alura, mas feito em C#.
 
 - Fazer chamada da API, método GET.
 - Armazenar a resposta em uma variável do tipo string
-- Criar uma classe com método que, usando expressões regulares, transforma o json em string e organiza as informações em um dicionário, com o nome do atributo e o valor do atributo.
+- Criar uma classe com método que, usando expressões regulares, transforma e organiza as informações da resposta em um dicionário, com o nome do atributo e o valor do atributo.
 - Exibir somente as informações selecionadas: titulo, link imagem e nota dos filmes.
 - API alternativa ao do IMDB, usando o top 10 melhores filmes.
 
+#### Desafios do Primeiro dia
+- Usar sua criatividade para deixar a saída dos dados mais bonitinha: usar emojis com código UTF-8, mostrar a nota do filme como estrelinhas, decorar o terminal com cores, negrito e itálico usando códigos ANSI, e mais!
+- Mudar o JsonParser para usar uma biblioteca de parsing de JSON -> Biblioteca usada `System.Text.Json.JsonSerializer`
+- Desafio supremo: criar alguma maneira para você dar uma avaliação ao filme, puxando de algum arquivo de configuração OU pedindo a avaliação para o usuário digitar no terminal.
